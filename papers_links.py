decc39ac46cec35e87d3c916bc4f96743bd78a01
livsguidetothegalaxy <36931577+livsguidetothegalaxy@users.noreply.github.com>
2020-05-15 16:45:11 -0700

    Code to pull up all references used

---
+++ papers_links.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May 15 15:39:15 2020
+
+@author: Olivia
+"""
+import os
+
+os.system("start \"\" https://arxiv.org/pdf/1903.02249.pdf")
+
+os.system("start \"\" https://arxiv.org/abs/2004.03701v1")
+
+os.system("start \"\" https://arxiv.org/abs/2001.10275")
